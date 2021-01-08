6d170ba90b0e4f4ca11c40c383de8d90c4499240
AlexDeLarge <jmrendon48@gmail.com>
2021-01-07 23:37:10 -0800

    added background change to task list dragging

--- assets/js/script.js
+++ assets/js/script.js
@@ -186,8 +186,8 @@ var dragTaskHandler = function(event) {
 var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
-      event.preventDefault();
-      
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
+        event.preventDefault();   
     }
 };
 
@@ -209,9 +209,16 @@ var dropTaskHandler = function(event) {
         statusSelectEl.selectedIndex = 2;
     }
     
+    dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
 };
 
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+};
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
@@ -222,3 +229,5 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
